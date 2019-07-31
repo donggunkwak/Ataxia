26a6a5cbbf0253bfdef3f10584723db4ef9fe2f7
donggunkwak <47698773+donggunkwak@users.noreply.github.com>
2019-07-30 21:20:20 -0700

    Changes to starting of line

--- scripts/testdiagonal2.js
+++ scripts/testdiagonal2.js
@@ -40,7 +40,14 @@ var canvas, ctx, flag = false,
         }
         
 
-        
+        function displayList(){
+            var e = "<hr/>";
+            e += " " + listX.length+" "+listY.length+ "<br/>";
+            for(var i = 0; i < listX.length; i++){
+                e += " " + listX[i] + " " + listY[i] + "<br/>";
+            }
+            document.getElementById("list").innerHTML = e;
+        }
         function draw() {
             ctx.beginPath();
             ctx.moveTo(prevX, prevY);
@@ -55,6 +62,7 @@ var canvas, ctx, flag = false,
             //console.log("Total Errors: " + total);
             //console.log("Total Pre Errors: " + pretotal);
             //console.log(currX + ", " + currY);
+
 
             // If Mouse Held Down
             if (res == 'down') {
@@ -80,16 +88,16 @@ var canvas, ctx, flag = false,
 
                         ctx.fillStyle = "#00FF00";
                         ctx.fillRect(leftObjective[0], leftObjective[1], leftObjective[2], leftObjective[3]);
-                }
+                    }
 
-                else {
-                    pretotal += 1;
-
-                    // Push Failed Attempts/Coords to List
-                    listX.push(currX);
-                    listY.push(currY);
-                    //console.log(currX + ", " + currY);
-                }
+                    else {
+                        pretotal += 1;
+                        return;
+                        // Push Failed Attempts/Coords to List
+                        listX.push(currX);
+                        listY.push(currY);
+                        displayList();
+                    }
 
                 }
 
@@ -121,7 +129,7 @@ var canvas, ctx, flag = false,
 
                     listX.push(currX);
                     listY.push(currY);
-                    //console.log(currX + ", " + currY);
+                    displayList();
 
                     // If End Checkpoint is Reached
                     if (currX >= rightObjective[0] && currX <= (rightObjective[0] + rightObjective[2]) && currY >= rightObjective[1] && currY <= (rightObjective[1] + rightObjective[3])) {
