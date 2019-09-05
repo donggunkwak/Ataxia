f8f6fcc42637747d44fca162fd0460a8c131b106
donggunkwak <47698773+donggunkwak@users.noreply.github.com>
2019-09-04 21:01:25 -0700

    Changes to horizontal test for refined functions
    added total fails, reset
    fixed reverses
    added own functions to all error checking

--- scripts/testhorizontal.js
+++ scripts/testhorizontal.js
@@ -1,6 +1,6 @@
 var canvas, ctx, flag = false,
             leftObjective = [10, 150, 50, 50],
-            rightObjective = [940, 150, 50, 50],
+            rightObjective = [1140, 150, 50, 50],
             prevX = 0,
             currX = 0,
             prevY = 0,
@@ -9,9 +9,9 @@ var canvas, ctx, flag = false,
             listY = [],
             pretotal = 0,
             total = 0,
+            fails = 0,
             out = 0,
             off = 0,
-            maxX = 0,
             reverse = 0,
             reverseStart = false,
             curOff = false,
@@ -26,12 +26,93 @@ var canvas, ctx, flag = false,
         var x = "black",
             y = 2;
         
+        function reset() {
+            canvas, ctx, flag = false,
+            leftObjective = [10, 150, 50, 50],
+            rightObjective = [1140, 150, 50, 50],
+            prevX = 0,
+            currX = 0,
+            prevY = 0,
+            currY = 0,
+            listX = [],
+            listY = [],
+            pretotal = 0,
+            total = 0,
+            fails = 0,
+            off = 0,
+            reverse = 0,
+            reverseStart = false,
+            curOff = false,
+            started = false,
+            dot_flag = false;
+            ended = false;
+            x = "black";
+            y = 2;
+            displayList();
+            init();
+            document.getElementById("discontinuities").innerHTML = "Discontinuities: "+total.toString();
+            document.getElementById("off").innerHTML = "Off-By-20: "+off.toString();
+            document.getElementById("reverses").innerHTML ="Reverses: "+reverse.toString();
+            
+            OptimalY = 0;
+        }
+        function discontinue()
+        {
+            if(started)
+                total+=1;
+            document.getElementById("discontinuities").innerHTML = "Discontinuities: "+total.toString();
+        }
+        function beforeStartMisses()
+        {
+            pretotal += 1;
+            fails+=1;
+            document.getElementById("misses").innerHTML = "Start fails: "+pretotal.toString(); 
+            document.getElementById("fails").innerHTML="Total Fails: "+fails.toString();
+        }
+        function outOfCanvas()
+        {
+            if(started)
+            {
+                out+=1;
+                fails+=1;
+                document.getElementById("outofcanvas").innerHTML = "OutOfCanvas: "+out.toString();
+                document.getElementById("fails").innerHTML="Total Fails: "+fails.toString();
+                reset();
+            }
+        }
+        function offBy20()
+        {
+            if(started&&Math.abs(currY-OptimalY)>20&&curOff==false)
+            {
+                curOff = true;
+                off+=1;
+                document.getElementById("off").innerHTML = "Off-By-20: "+off.toString();
+            }
+            if(curOff==true&&Math.abs(currY-OptimalY)<=20)
+            {
+                curOff=false;
+            }
+        }
+        function reversef()
+        {
+           // reset();
+            if(currX>prevX)
+            {
+                reverseStart = false;
+            }
+            else if(reverseStart==false&&currX<prevX)
+            {
+                reverseStart = true;
+                reverse+=1;
+                document.getElementById("reverses").innerHTML ="Reverses: "+reverse.toString();
+            }
+        }
+
         function init() {
             canvas = document.getElementById('can');
             ctx = canvas.getContext("2d");
             w = canvas.width;
             h = canvas.height;
-
             ctx.fillStyle = "#FF0000";
             ctx.fillRect(leftObjective[0], leftObjective[1], leftObjective[2], leftObjective[3]);
             ctx.fillRect(rightObjective[0], rightObjective[1], rightObjective[2], rightObjective[3]);
@@ -69,10 +150,6 @@ var canvas, ctx, flag = false,
             ctx.closePath();
         }
         function findxy(res, e) {
-            //console.log("List Length: " + listX.length);
-            //console.log("Total Errors: " + total);
-            //console.log("Total Pre Errors: " + pretotal);
-            //console.log(currX + ", " + currY);
             if(ended)
                 return;
 
@@ -82,7 +159,6 @@ var canvas, ctx, flag = false,
                 prevY = currY;
                 currX = e.clientX - canvas.offsetLeft;
                 currY = e.clientY - canvas.offsetTop;
-                maxX= Math.max(maxX,currX);
                 /*
                 10,150
                 10,200
@@ -97,15 +173,14 @@ var canvas, ctx, flag = false,
                     // If Click Button
                     if (currX >= leftObjective[0] && currX <= (leftObjective[0] + leftObjective[2]) && currY >= leftObjective[1] && currY <= (leftObjective[1] + leftObjective[3])) {
                         started = true;
-
+                        OptimalY = currY;
                         ctx.fillStyle = "#00FF00";
                         ctx.fillRect(leftObjective[0], leftObjective[1], leftObjective[2], leftObjective[3]);
-                        OptimalY = currY;
+                        
                     }
 
                     else {
-                        pretotal += 1;
-                        document.getElementById("misses").innerHTML = "Before-Start misses: "+pretotal.toString(); 
+                        beforeStartMisses();
                         listX.push(currX);
                         listY.push(currY);
                         displayList();
@@ -114,12 +189,7 @@ var canvas, ctx, flag = false,
                     }
 
                 }
-                if(started&&Math.abs(currY-OptimalY)>20&&curOff==false)
-                {
-                    curOff = true;
-                    off+=1;
-                    document.getElementById("off").innerHTML = "Off-By-20: "+off.toString();
-                }
+                offBy20();
                 if(curOff==true&&started&&Math.abs(currY-OptimalY)<=20)
                 {
                     curOff=false;
@@ -141,17 +211,12 @@ var canvas, ctx, flag = false,
             // If Mouse Not Held Down
             if (res == 'up') {
                 flag = false;
-                if(started)total += 1;
-                document.getElementById("discontinuities").innerHTML = "Discontinuities: "+total.toString();
+                discontinue();
             }
             if(res=='out')
             {
                 flag = false;
-                if(started)
-                {
-                    out+=1;
-                    document.getElementById("outofcanvas").innerHTML = "OutOfCanvas: "+out.toString();
-                }
+                outOfCanvas();
             }
 
             // If Mouse Moves
@@ -161,11 +226,7 @@ var canvas, ctx, flag = false,
                     prevY = currY;
                     currX = e.clientX - canvas.offsetLeft;
                     currY = e.clientY - canvas.offsetTop;
-                    if(currX>maxX)
-                    {
-                        maxX = currX;
-                        reverseStart = false;
-                    }
+
 
                     listX.push(currX);
                     listY.push(currY);
@@ -179,26 +240,8 @@ var canvas, ctx, flag = false,
 
                     }
                     draw();
-
-
-
-                    if(Math.abs(currY-OptimalY)>20&&curOff==false)
-                    {
-                        curOff = true;
-                        off+=1;
-                        document.getElementById("off").innerHTML = "Off-By-20: "+off.toString();
-                    }
-                    if(curOff==true&&Math.abs(currY-OptimalY)<=20)
-                    {
-                        curOff=false;
-                    }
-                    if(reverseStart==false&&currX<maxX)
-                    {
-                        reverseStart = true;
-                        reverse+=1;
-                        document.getElementById("reverses").innerHTML ="Reverses: "+reverse.toString();
-                    }
-                    
+                    offBy20();
+                    reversef();
                 }
             }
         }
